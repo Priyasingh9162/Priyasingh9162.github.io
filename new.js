47ee83c0c5ee744df8697336c05c2ae8a5e84537
Priyasingh9162 <60290391+Priyasingh9162@users.noreply.github.com>
2020-05-08 23:52:15 -0700

    Add files via upload

--- new.js
+++ new.js
@@ -1,6 +1,16 @@
 document.write("<h1><b><u>Hello World in Module4 With Java Script</u></b></h1>");
 document.write("<b> Check the Emlimentation of object with console---</b><br>");
 document.write("<b> Check the Emlimentation of function with console---</b>");
+console.log("Hello yaakov");
+console.log("Good bye john");
+console.log("Good bye jen");
+console.log("Hello priya");
+console.log("Hello larry");
+console.log("Good bye jim ");
+console.log("Hello shalu");
+console.log("Bye Bye shalu");
+console.log("Hello jiya");
+console.log("Good bye jiya");
 var a={
 	firstName :"Priya",
 	lastName :"Kumari",
