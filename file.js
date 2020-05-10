3ae62b6e8821971c180233f4c26a175e99ec5271
Priyasingh9162 <60290391+Priyasingh9162@users.noreply.github.com>
2020-05-09 23:32:31 -0700

    Add files via upload

---
+++ file.js
@@ -0,0 +1,29 @@
+function hey(){
+	var a= document.getElementById("one").value
+	document.write("Lunch <br>");
+	document.write("Dinner <br>");
+	document.write("Susi");
+}
+function hello(){
+	var b= document.getElementById("two").value
+	document.write("<b><u>Special in Lunch</u></b><br>");
+	document.write("Dosa<br>");
+	document.write("Fried rice<br>");
+	document.write("Biryani<br>");
+	document.write("Rajma rice<br>");
+	document.write("<b><u>Special in Dinner</u></b><br>");
+	document.write("Dosa<br>");
+	document.write("Fried rice<br>");
+	document.write("Biryani<br>");
+	document.write("Rajma rice<br>");
+	document.write("<b><u>Special in Susi</u></b><br>");
+	document.write("Dosa<br>");
+	document.write("Fried rice<br>");
+	document.write("Biryani<br>");
+	document.write("Rajma rice<br>");
+
+}
+function hy(){
+	var c= document.getElementById("three").value
+	document.write("Here you can get the map of the location");
+}
