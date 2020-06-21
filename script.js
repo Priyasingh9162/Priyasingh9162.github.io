b86b597337ba42f4a08a70f8e8c0c935cbe7e531
Priyasingh9162 <60290391+Priyasingh9162@users.noreply.github.com>
2020-06-21 07:35:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+document.getElementById('myform').addEventListener('submit',saveBookmark);
+function saveBookmark(e){
+	
+var siteName =document.getElementById('siteName').value;
+var siteUrl =document.getElementById('siteUrl').value;
+
+var bookmark = {
+
+     name: siteName,
+     url:  siteUrl
+}
+
+if(localStorage.getItem('bookmarks') === null){
+	var bookmarks = [];
+	bookmarks.push(bookmark);
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+}
+else{
+	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+	bookmarks.push(bookmark);
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+}
+
+    fetchBookmarks();
+	e.preventDefault();
+}
+
+function deleteBookmark(url){
+
+	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+	for(var i=0;i<bookmarks.length;i++){
+		if(bookmarks[i].url == url){
+			bookmarks.splice(i,i);
+		}
+
+	}
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	fetchBookmarks();
+
+}
+
+function fetchBookmarks(){
+	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    var bookmarksResults = document.getElementById('bookmarksResults');
+    bookmarksResults.innerHTML = '';
+    for(var i=0;i<bookmarks.length;i++){
+    	var name = bookmarks[i].name;
+    	var url = bookmarks[i].url;
+    	bookmarksResults.innerHTML +='<div class="well">'+
+    	                               '<h3>'+name+
+    	                               '<a class="btn btn-default" target="_blank" href="'+url+'">Visit</a>'+
+    	                               '<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger"  href="#">Delete</a>'
+    	                               '</h3>'+
+    	                               '</div>';
+
+    }
+}
